f3f4d1b8ae6f6d907d406798faa4ebbb36e2498b
Adam Peacock <adam@thepeacock.net>
2021-01-13 21:25:11 -0500

    Admin optimizations

--- dbqueue/admin.py
+++ dbqueue/admin.py
@@ -1,15 +1,44 @@
 """ Admin helpers for database queue """
 
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
+from django.utils.translation import gettext_lazy as _
 
 from . import models
+
+
+class JobFinishedFilter(admin.SimpleListFilter):
+    """ Simple filter to indicate if jobs have finished """
+
+    title = _("job finished")
+    parameter_name = "finished"
+
+    def lookups(self, request, model_admin):
+        return (
+            ("success", _("Successfully")),
+            ("failure", _("Unsuccessfully")),
+            ("unknown", _("Not completed")),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == "success":
+            return queryset.filter(final_result__success=True)
+
+        if self.value() == "failure":
+            return queryset.filter(final_result__success=False)
+
+        if self.value() == "unknown":
+            return queryset.filter(final_result__isnull=True)
+
+        return queryset
 
 
 @admin.register(models.Job)
 class JobAdmin(admin.ModelAdmin):
     """ Job admin """
 
-    list_filter = ["final_result__success"]
+    list_filter = [JobFinishedFilter]
     list_display = ["id", "__str__", "queued_at", "priority", "finished"]
 
     def finished(self, obj: models.Job):
@@ -19,3 +48,6 @@ class JobAdmin(admin.ModelAdmin):
         return obj.final_result.success
 
     finished.boolean = True
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        return super().get_queryset(request).select_related("final_result")
