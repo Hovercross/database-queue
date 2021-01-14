648dcb35ec532f5dfea120b93c69145835ec5a3b
Adam Peacock <adam@thepeacock.net>
2021-01-13 21:18:34 -0500

    Few things onto admin

--- dbqueue/admin.py
+++ dbqueue/admin.py
@@ -10,3 +10,12 @@ class JobAdmin(admin.ModelAdmin):
     """ Job admin """
 
     list_filter = ["final_result__success"]
+    list_display = ["id", "__str__", "queued_at", "priority", "finished"]
+
+    def finished(self, obj: models.Job):
+        if not obj.final_result:
+            return None
+
+        return obj.final_result.success
+
+    finished.boolean = True
